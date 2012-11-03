23e8bd9c2f4596c933de02c27b9e805f726c10c6
Rob Middleton <rob@i.tv>
2012-11-03 15:42:03 -0600

    rewrite

--- cache.js
+++ cache.js
@@ -1,66 +1,65 @@
-exports.Minute = 60
-exports.Hour = 60*exports.Minute
-exports.Day = 24*exports.Hour
-exports.Week = 7*exports.Day
-exports.Month = 30*exports.Day
+var _ = require("underscore")
 
-exports.NoCache = "NoCache" // Only used as a constant
+var headerKey = 'Cache-Control'
+var noCacheKey = 'no-cache'
+var maxAgeKey = 'max-age'
+var mustRevalidateKey = 'must-revalidate'
 
-exports.HeaderKey = 'Cache-Control'
-exports.NoCacheKey = 'no-cache'
-exports.MaxAgeKey = 'max-age'
-exports.MustRevalidateKey = 'must-revalidate'
+var units = {}
+units.second = 1
+units.minute = units.second * 60
+units.hour = units.minute * 60
+units.day = units.hour * 24
+units.week = units.day * 7
+units.month = units.day * 30
+units.year = units.day * 365
 
-// Cache-Control: max-age=3600, must-revalidate
-// Cache-Control: no-cache
-// max age is in seconds
+// add plural units
+Object.keys(units).forEach(function (unit) {
+    units[unit + "s"] = units[unit]
+})
 
-exports.forADay    = function(response) { exports.cache(response, exports.Day) }
-exports.forAWeek   = function(response) { exports.cache(response, exports.Week) }
-exports.forAnHour  = function(response) { exports.cache(response, exports.Hour) }
-exports.forAMonth  = function(response) { exports.cache(response, exports.Month) }
+var middleware = function (seconds) {
+    return function (req, res, next) {
+        console.log("seconds", seconds)
+        if (seconds === 0) {
+            res.header(headerKey, noCacheKey)
+        }
+        else {
+            res.header(headerKey, maxAgeKey + "=" + seconds + ", " + mustRevalidateKey)
+        }
 
-
-exports.cache = function(response, value) {
-    
-    // sends down the right headers to cache an express
-    // response object
-
-    if (value == exports.NoCache) return exports.noCache(response)
-    
-    response.header(exports.HeaderKey, exports.MaxAgeKey + "=" + value + ", " + exports.MustRevalidateKey)
+        next()
+    }
 }
 
-exports.forSeconds = exports.cache
+function CacheControl (options) {
+    if (!options) options = {}
 
-exports.noCache = function(response) {
-    
-    // specifies not to cache this response
-    response.header(exports.HeaderKey, exports.NoCacheKey)
-    
+    this.override = options.override
+
+    this.middleware = _.bind(this.middleware, this)
 }
 
-exports.dontCache = function(req, res, next) {
-	res.header('Cache-control', 'no-cache')
-	next()
+CacheControl.prototype = {
+    calculate: function (unit, value) {
+        if (unit === 0 || value === 0 || unit === false) return middleware(0)
+
+        var unitValue = units[unit]
+        if (!unitValue) throw new Error("CacheControl unknown unit " + unit)
+
+        if (this.override !== undefined) {
+            return this.override
+        }
+
+        if (!value) value = 1 // default to 1 (unless it is 0 which we already checked) so they can cache("day")
+
+        return unitValue * value
+    },
+
+    middleware: function (unit, value) {
+        return middleware(this.calculate(unit, value))
+    }
 }
 
-exports.cacheHourly = function(req, res, next) {
-    exports.cache(res, exports.Hour)	    
-    next()
-}
-
-exports.cacheDaily = function(req, res, next) {
-    exports.cache(res, exports.Day)	    
-    next()
-}
-
-exports.cacheMonthly = function(req, res, next) {
-    exports.cache(res, exports.Month)	    
-    next()
-}
-
-exports.cacheMinute = function(req, res, next) {
-    exports.cache(res, exports.Minute)
-    next()
-}
+module.exports = CacheControl
